f726a995fcad1eaa877afb9d75d9cb28c25956c7
aclibbon <alex.clibbon@gmail.com>
2015-03-24 11:18:29 +0000

    Response view added

--- Listener/urls.py
+++ Listener/urls.py
@@ -4,4 +4,5 @@ from Listener import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'response', views.text_test, name='response'),
 )
--- Listener/views.py
+++ Listener/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
+import twilio.twiml
 
 # Create your views here.
 def index(request):
     return HttpResponse(request.path)
+
+def text_test(request):
+    resp = twilio.twiml.Response()
+    resp.message("A successful response")
+    return HttpResponse(resp)
 
